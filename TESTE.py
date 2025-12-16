4d483afe04f61cff00e97b5d04ba4c6ff7b63d3a
Lucas Dos Santos <lukasdossantos7@gmail.com>
2025-12-16 00:05:49 -0300

    Commit

---
+++ TESTE.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+caminho_excel = r"C:\\Users\\Pichau\\Desktop\\TESTE DE NOVO\\Teste 01 - Analista de Controle Financeiro Jr.xlsm"
+
+Visualizacao = pd.read_excel(caminho_excel)
+
+print(Visualizacao)
